8617209318ae7a04388572af793f9f1255bcf3dc
cyril <cyril.carlier@gmail.com>
2020-01-21 15:45:05 +0100

    mise en commentaire des ajouts de donnees

--- init.sql
+++ init.sql
@@ -9,14 +9,14 @@ CREATE TABLE IF NOT EXISTS `gare` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 
-INSERT INTO `gare` (`ID_gare`, `Nom_gare`) VALUES
+/*INSERT INTO `gare` (`ID_gare`, `Nom_gare`) VALUES
 (1, 'Lyon'),
 (2, 'Paris'),
 (3, 'Nantes'),
 (4, 'Marseille'),
 (5, 'Veauche'),
 (6, 'Tassin');
-
+*/
 
 CREATE TABLE IF NOT EXISTS `train` (
   `ID_train` int(10) NOT NULL,
@@ -30,8 +30,8 @@ CREATE TABLE IF NOT EXISTS `train` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 `ID_gare`
 
-INSERT INTO `train` (`ID_train`, `Heure_départ`, `Heure_arrivée`, `ID_gare_départ`, `ID_gare_arrivée`) VALUES
+/*INSERT INTO `train` (`ID_train`, `Heure_départ`, `Heure_arrivée`, `ID_gare_départ`, `ID_gare_arrivée`) VALUES
 (1000, '2020-01-21 08:00:00', '2020-01-21 10:00:00', 1, 2),
 (1010, '2020-01-21 08:30:00', '2020-01-21 09:45:00', 6, 1);
-
+*/
 COMMIT;
