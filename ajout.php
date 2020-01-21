20e17c150aba8b5fedd36513d354a9d6e856174a
cyrilCarlier <58560634+cyrilCarlier@users.noreply.github.com>
2020-01-21 14:13:30 +0100

    update ajout.php

---
+++ ajout.php
@@ -0,0 +1,20 @@
+<?php
+$servername = "10.69.8.45";
+$username = "adel";
+$password = "adel";
+$dbname = "stage";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+}
+
+$Xnom = $_POST['prenom'];
+$Xprenom = $_POST['nom'];
+
+$conn->query("INSERT INTO stagiaire (nom, prenom) VALUES ('$Xnom', '$Xprenom')");
+
+$conn->close();
+?>
